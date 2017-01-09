1d5cabee430ce6c874dca2683af1dddc736c1cd6
Mofl2328 <mailfds@mofl.de>
2017-01-09 15:22:55 +0100

    Some functionality
    still mostly testing

--- Classes/RootLuisDialog.cs
+++ Classes/RootLuisDialog.cs
@@ -9,13 +9,47 @@ using System.Web;
 
 namespace movl_test_bot
 {
-    [LuisModel("2c60d7a6-cadc-42cd-9116-64f67c2bea7d", "34593796f7524f69b4592a14bfa8aa94")]
+
+    [LuisModel("2bbf5697-9b51-45fb-bb4c-e2f66ca2f416", "34593796f7524f69b4592a14bfa8aa94")]
     public class RootLuisDialog : LuisDialog<object>
     {
         [LuisIntent("Hilfe")]
         public async Task HelpDialoge(IDialogContext context, LuisResult result)
         {
             await context.PostAsync("Ich war zwar in der Lage es zu verstehen kann aber nicht helfen.");
+
+        }
+
+        [LuisIntent("Auskunft")]
+        public async Task AuskunftDialoge(IDialogContext context, LuisResult result)
+        {
+            foreach (var entity in result.Entities)
+            {
+                switch (entity.Type)
+                {
+                    case "ServiceKeyword":
+
+                        break;
+                    case "Funktion":
+
+                        break;
+                    case "Person":
+
+                        break;
+                    case "Zeitraum":
+                        await context.PostAsync("Test: " + entity.Entity.ToLower());
+                        break;
+                    case "Zeiteinheit":
+
+                        break;
+                    case "Anzahl":
+
+                        break;
+                }
+                //await context.PostAsync("Test: " + entity.Type + entity.Entity.ToLower());
+
+            }
+
 
         }
 
---
+++ classes/KeyWords.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace movl_test_bot
+{
+    public static class KeyWords
+    {
+        public static string[] statusWords = { "ist", "war", "geht", "ging" };
+    }
+}
